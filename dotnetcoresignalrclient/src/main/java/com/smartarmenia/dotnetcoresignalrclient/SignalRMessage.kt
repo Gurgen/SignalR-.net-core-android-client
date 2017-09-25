8e3160431be70706e19d26be1a55a2c201c9112d
gurgen <hakobyan.gurgen@gmail.com>
2017-09-25 20:46:39 +0400

    Compile to implementation

--- dotnetcoresignalrclient/src/main/java/com/smartarmenia/dotnetcoresignalrclient/SignalRMessage.kt
+++ dotnetcoresignalrclient/src/main/java/com/smartarmenia/dotnetcoresignalrclient/SignalRMessage.kt
@@ -5,7 +5,7 @@ import com.google.gson.JsonElement
 class SignalRMessage {
     var invocationId: String? = null
     var type: Int? = null
-    var target:String? = null
+    var target: String? = null
     var nonBlocking: Boolean? = null
-    var arguments:Array<JsonElement>? = null
+    var arguments: Array<JsonElement>? = null
 }
